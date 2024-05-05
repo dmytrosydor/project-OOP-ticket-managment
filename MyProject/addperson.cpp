5118bee7bab6146058690b4e4e638de7c9584bd0
Dmytro Sydor <dmytro.sydor.shi.2023@lpnu.ua>
2024-05-05 21:34:57 +0300

    aded add person function

--- MyProject/addperson.cpp
+++ MyProject/addperson.cpp
@@ -1,5 +1,6 @@
 #include "addperson.h"
 #include "ui_addperson.h"
+#include "dbHeader.h"
 
 AddPerson::AddPerson(QWidget *parent)
     : QDialog(parent)
@@ -17,5 +18,39 @@ void AddPerson::on_btnSave_clicked()
 {
     QString firstname = ui->txtFristName->text();
     QString middlename = ui->txtMiddleName->text();
+    QString lastname = ui->txtLastName->text();
+    QString passport = ui->txtPassportNuber->text();
+    QString contactN = ui->txtContactNumber->text();
+    QString adress = ui->txtAdress->text();
+    QString destination = ui->cmbChooseDestination->currentText();
+
+    QSqlDatabase database = QSqlDatabase::addDatabase("QSQLITE");
+    database.setDatabaseName("/Users/user/Desktop/project/database/Myproject.db");
+
+    if(!database.open()){
+        qDebug() << "Unable to open database";
+        return;
+    }
+    else {
+        qDebug() << "Database opened successfully";
+    }
+
+    QSqlQuery query(database);
+    query.prepare("INSERT INTO Person (FirstName, MiddleName, LastName, Passport, Contact, Adress, Ticket) VALUES (?, ?, ?, ?, ?, ?, ?)");
+    query.bindValue(0, firstname);
+    query.bindValue(1, middlename);
+    query.bindValue(2, lastname);
+    query.bindValue(3, passport);
+    query.bindValue(4, contactN);
+    query.bindValue(5, adress);
+    query.bindValue(6, destination);
+
+    if (!query.exec()) {
+        qDebug() << "Error executing query:" << query.lastError().text();
+    } else {
+        qDebug() << "Data inserted successfully";
+    }
+
+    database.close();
 }
 
